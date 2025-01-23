660a4e49a51ed2104e0bade30fc593e184cfc47b
yowww1094 <younesaitsiabou@gmail.com>
2025-01-23 16:07:48 +0100

    finished dashboard design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,9 +8,15 @@ import interactionPlugin from "@fullcalendar/interaction"
 
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+
 
 import { BsFilePerson } from "react-icons/bs";
 import { FaRegEye } from "react-icons/fa";
+import { IoCloseSharp } from "react-icons/io5";
+import { IoIosSearch } from "react-icons/io";
+
 
 
 const CARD_ITEMS = [
@@ -49,6 +55,80 @@ const PIE_CHART_DATA = [
 
 const PIE_CHART_COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 function Home() {
   function handleEventClick(arg) {
@@ -118,7 +198,7 @@ function Home() {
                   <th className='text-[12px] uppercase tracking-wide text-gray-400 px-4 py-2 text-left bg-gray-50 rounded-tl-md rounded-bl-md'>Date</th>
                   <th className='text-[12px] uppercase tracking-wide text-gray-400 px-4 py-2 text-left bg-gray-50'>Patient</th>
                   <th className='text-[12px] uppercase tracking-wide text-gray-400 px-4 py-2 text-left bg-gray-50'>Appiontement Type</th>
-                  <th className='text-[12px] uppercase tracking-wide text-gray-400 px-4 py-2 text-left bg-gray-50 rounded-tr-md rounded-br-md'>Action</th>
+                  <th className='text-[12px] uppercase tracking-wide text-gray-400 px-4 py-2 text-left bg-gray-50 rounded-tr-md rounded-br-md'></th>
                 </tr>
               </thead>
               <tbody>
@@ -134,7 +214,77 @@ function Home() {
             </table>
           </div>
         </div>
+        <div className="bg-white border border-gray-100 shadow-md shadow-black/10 p-6">
+          <div className="flex justify-between items-center mb-4">
+            <div className="font-medium">Patients Payements</div>
+            <Link className="font-light text-[11px] text-blue-500 hover:text-blue-700 hover:underline" to='/patients'>View More</Link>
+          </div>
+          <div className="flex items-center mb-4">
+            <div className={classNames(' w-full bg-white rounded-md  ml-auto flex items-center')}>
+              <form action="" className='p-1 w-full'>
+                <div className="relative w-full">
+                  <input type="text" className='pr-4 pl-10 py-2 bg-gray-50 w-full outline-none border border-gray-100 rounded-md text-sm focus:border-blue-500' placeholder='Search...'/>
+                  <button type='submit' className='absolute top-1/2 left-1.5 -translate-y-1/2 text-gray-400 h-8 w-8 rounded flex items-center justify-center hover:text-gray-600'><IoIosSearch /></button>
+                </div>
+              </form>
+              <select className='text-sm text-gray-600 px-4 py-2 bg-gray-50 border border-gray-100 rounded focus:border-blue-500 outline-none appearance-none'>
+                <option value="">All</option>
+                <option value="">All</option>
+                <option value="">All</option>
+              </select>
+            </div>
+          </div>
+          <div className="overflow-x-auto">
+            <table className='w-full min-w-[540px]'>
+              <thead>
+                <tr>
+                  <th className='text-[12px] uppercase tracking-wide text-gray-400 px-4 py-2 text-left bg-gray-50 rounded-tl-md rounded-bl-md'>Patient</th>
+                  <th className='text-[12px] uppercase tracking-wide text-gray-400 px-4 py-2 text-left bg-gray-50'>Total</th>
+                  <th className='text-[12px] uppercase tracking-wide text-gray-400 px-4 py-2 text-left bg-gray-50'>Last Payement</th>
+                  <th className='text-[12px] uppercase tracking-wide text-gray-400 px-4 py-2 text-left bg-gray-50 rounded-tr-md rounded-br-md'>Last Payement Date</th>
+                  <th className='text-[12px] uppercase tracking-wide text-gray-400 px-4 py-2 text-left bg-gray-50 rounded-tr-md rounded-br-md'></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className='py-2 px-4'><span className='text-[13px] font-medium text-gray-400'>Younes</span></td>
+                  <td className='py-2 px-4'><span className='text-[13px] font-medium text-gray-400'>2500 DH</span></td>
+                  <td className='py-2 px-4'><span className='text-[13px] font-medium text-gray-400'>1000 DH</span></td>
+                  <td className='py-2 px-4'><span className='text-[13px] font-medium text-gray-400'>20/12/2025</span></td>
+                  <td className='py-2 px-4 flex items-center'>
+                    <button className='items-center justify-center inline-block p-1 rounded bg-emerald-500/10 text-emerald-500 font-semibold text-[16px]'><FaRegEye /></button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2/3">
+        <div className='flex flex-row justify-start w-full bg-white border border-gray-100 shadow-md shadow-black/10 p-6'>
+          <ResponsiveContainer width={"100%"} height={400}>
+            <BarChart
+              data={data}
+              margin={{
+                top: 20,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+              <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+
     </div>
   )
 }
